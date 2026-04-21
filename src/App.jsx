28795515c326f45b7ece15b01cc56232a6f5a816
Guillermo de la Torre Isunza <memo2507.delatorre@gmail.com>
2026-04-20 20:09:57 -0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { socket } from '../socket'
+import { socket } from './socket'
 import ManageConnection from './components/ManageConnection'
 import MyForm from './components/MyForm'
 import Channels from './components/Channels'
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { socket } from '../../socket'
+import { socket } from '../socket'
 
 
 function Chats() {
